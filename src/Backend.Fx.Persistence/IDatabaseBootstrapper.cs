6d7fa1e56ec88ed817a782cd690359b99fa973e9
Marc Wittke <marc@wittke-web.de>
2024-01-05 12:10:44 +0100

    IDatabaseBootstrapper

---
+++ src/Backend.Fx.Persistence/IDatabaseBootstrapper.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.Fx.Persistence
+{
+    /// <summary>
+    /// Encapsulates database bootstrapping. This interface hides the implementation details for creating/migrating the database
+    /// </summary>
+    public interface IDatabaseBootstrapper : IDisposable
+    {
+        Task EnsureDatabaseExistenceAsync(CancellationToken cancellationToken);
+    }
+}
