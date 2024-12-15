cdd402767fb3eaf64275be8cc30b8df6ee427c38
Marc Wittke <marc@wittke-web.de>
2024-12-15 23:56:55 +0100

    another approach for Nodatime support in npgsql

--- src/Backend.Fx.Persistence.Postgres/PostgresConnectionFactory.cs
+++ src/Backend.Fx.Persistence.Postgres/PostgresConnectionFactory.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Data;
 using Backend.Fx.Persistence.AdoNet;
 using JetBrains.Annotations;
@@ -11,18 +12,23 @@ public class PostgresConnectionFactory : IDbConnectionFactory
     private readonly NpgsqlConnectionStringBuilder _connectionStringBuilder;
     private readonly NpgsqlDataSource _dataSource;
 
-    public PostgresConnectionFactory(NpgsqlConnectionStringBuilder connectionStringBuilder)
+    public PostgresConnectionFactory(
+        NpgsqlConnectionStringBuilder connectionStringBuilder,
+        Action<NpgsqlDataSourceBuilder>? configure = null)
     {
         _connectionStringBuilder = connectionStringBuilder;
         var dataSourceBuilder = new NpgsqlDataSourceBuilder(_connectionStringBuilder.ConnectionString);
-        dataSourceBuilder.UseNodaTime();
+        configure?.Invoke(dataSourceBuilder);
         _dataSource = dataSourceBuilder.Build();
     }
+
+    public virtual void ConfigureDataSource(NpgsqlDataSourceBuilder dataSourceBuilder)
+    { }
 
     public string ConnectionString => _connectionStringBuilder.ConnectionString;
 
     public IDbConnection Create()
     {
-        return _dataSource.OpenConnection();
+        return _dataSource.CreateConnection();
     }
 }
